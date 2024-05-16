fd666ab3250a08391cd8617109f14c4491a2a75d
felix delgado <felix.ivan91@gmail.com>
2024-05-15 19:17:07 -0600

    create next.js project and add landing page doc

---
+++ my-app/pages/landing.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const HomePage = () => {
+    return (
+        <div>
+            <iframe src="/landing.html" width="100%" height="600 px" />
+        </div>
+    );
+}
+
+export default HomePage;
